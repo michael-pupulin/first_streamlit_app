890a548aee3712abb2e89518d90a4b95fd04ac0f
Michael Pupulin <54189361+michael-pupulin@users.noreply.github.com>
2023-01-06 14:29:24 -0500

    Update streamlit_app.py
    Tried to add breakfast menu.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title("My Parent's New Healthy Diner")
+
+streamlit.subheader("Breakfest")
+streamlit.latex("
+                \begin{itemize}
+                \item \textbf{Three egg omlet} \textit{Add peppers, cheese tomato.} 7.99
+                \item \textbf{Bacon sandwhich} \textit{Add cheese, comes with fries.} 8.99
+                \end{itemize}
+                ")
