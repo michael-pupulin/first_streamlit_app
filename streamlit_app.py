45bd6fb6dc3d37048da2636ad620ecba1e8b2976
Michael Pupulin <54189361+michael-pupulin@users.noreply.github.com>
2023-01-06 14:30:08 -0500

    Update streamlit_app.py
    Tried to fix latex.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit
 streamlit.title("My Parent's New Healthy Diner")
 
 streamlit.subheader("Breakfest")
-streamlit.latex("
+streamlit.latex(r'''
                 \begin{itemize}
                 \item \textbf{Three egg omlet} \textit{Add peppers, cheese tomato.} 7.99
                 \item \textbf{Bacon sandwhich} \textit{Add cheese, comes with fries.} 8.99
                 \end{itemize}
-                ")
+                ''')
